062a9b6e2978df2b3b6225eda71885aabcb4a2f4
Jhon Álvarez <jwalvarez.98@gmail.com>
2022-05-22 12:54:04 -0500

    Added Course Card component, changed page icon

---
+++ src/components/CourseCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <a href="#" class="w-full block h-full border bg-base-100/5 border-black/30 rounded-xl">
+    <div class="relative">
+      <img alt="blog photo" :src="img" class="relative rounded-t-xl max-h-40 w-full object-cover" />
+      <div class="absolute top-0 left-0 ml-2 flex flex-wrap justify-starts items-center my-2">
+        <div v-for="tag in tags"
+          class="text-sm mr-1 py-1.5 px-4 text-white hover:text-success bg-black/20 rounded-2xl backdrop-blur-md">
+          {{ tag }}
+        </div>
+      </div>
+    </div>
+    <div class=" w-full p-4">
+      <h2 class="text-base-100 text-xl font-black overflow-hidden whitespace-nowrap text-ellipsis w-auto">
+        {{ title }}
+      </h2>
+      <div class="flex-grow border-t border-base-100/10 my-2"></div>
+      <span class="flex justify-between my-2">
+        <div class="flex">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+            class="bi bi-clock text-base-100 mr-2 my-auto" viewBox="0 0 16 16">
+            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+          </svg>
+          <p class="text-sm text-base-100 font-medium my-auto">{{ startDate }}</p>
+        </div>
+        <div class="avatar-group -space-x-4 flex">
+          <div class="avatar">
+            <div class="w-8 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=3174" />
+            </div>
+          </div>
+          <div class="avatar">
+            <div class="w-8 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=3194" />
+            </div>
+          </div>
+        </div>
+      </span>
+      <p class="text-base-100/40 text-md mb-4 course-description">
+        {{ description }}
+      </p>
+      <CourseButton label="Ver Curso" />
+    </div>
+  </a>
+</template>
+<script>
+import CourseButton from '../base/CourseButton.vue';
+export default {
+  props: {
+    title: String,
+    img: String,
+    startDate: String,
+    description: String,
+    tags: Array,
+  },
+  components: { CourseButton }
+}
+</script>
+<style lang="">
+  
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,63 +1,19 @@
 <script setup>
 import Banner from "./Banner.vue";
-import CourseButton from "../base/CourseButton.vue";
+import CourseCard from "./CourseCard.vue";
 </script>
+
 <template>
   <Banner />
-  <h1 class="text-4xl text-base-100 font-black px-[10%]">Cursos</h1>
+  <h1 class="text-4xl text-base-100 font-black px-[10%]">Cursos de trading</h1>
   <div class="flex py-4 space-x-6 mx-[10%] rounded-box overflow-x-scroll">
-    <div v-for="course in trading_courses" class="carousel-item shadow-lg rounded-2xl h-90 w-[300px] m-auto">
-      <a href="#" class="w-full block h-full border bg-base-100/5 border-black/30 rounded-xl">
-        <div class="relative">
-          <img alt="blog photo" :src="course.img" class="relative rounded-t-xl max-h-40 w-full object-cover" />
-          <div class="absolute top-0 left-0 ml-2 flex flex-wrap justify-starts items-center my-2">
-            <div
-              class="text-sm mr-1 py-1.5 px-4 text-white hover:text-success bg-black/20 rounded-2xl backdrop-blur-md">
-              #Crypto
-            </div>
-            <div
-              class="text-sm mr-1 py-1.5 px-4 text-white hover:text-success bg-black/20 rounded-2xl backdrop-blur-md">
-              #Trading
-            </div>
-          </div>
-        </div>
-        <div class=" w-full p-4">
-          <h2 class="text-base-100 text-xl font-black overflow-hidden whitespace-nowrap text-ellipsis w-auto">
-            {{ course.title }}
-          </h2>
-          <div class="flex-grow border-t border-base-100/10 my-2"></div>
-          <span class="flex justify-between my-2">
-            <div class="flex">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-clock text-base-100 mr-2 my-auto" viewBox="0 0 16 16">
-                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-              </svg>
-              <p class="text-sm text-base-100 font-medium my-auto"> Inicio: Agosto, 2022</p>
-            </div>
-            <div class="avatar-group -space-x-4 flex">
-              <div class="avatar">
-                <div class="w-8 rounded-full">
-                  <img src="https://api.lorem.space/image/face?hash=3174" />
-                </div>
-              </div>
-              <div class="avatar">
-                <div class="w-8 rounded-full">
-                  <img src="https://api.lorem.space/image/face?hash=3194" />
-                </div>
-              </div>
-            </div>
-          </span>
-          <p class="text-base-100/40 text-md mb-4 course-description">
-            {{ course.description }}
-          </p>
-          <CourseButton label="Ver Curso" />
-        </div>
-      </a>
+    <div v-for="course in trading_courses"
+      class="carousel-item shadow-xl rounded-2xl h-90 w-[300px] m-auto hover:-translate-y-2 duration-300">
+      <CourseCard :img="course.img" :title="course.title" :description="course.description"
+        :startDate="course.startDate" :tags="course.tags" />
     </div>
   </div>
   <div class="h-[200px]"></div>
-
 </template>
 
 <script>
@@ -72,30 +28,40 @@ export default {
           description: "Nunc malesuada euismod lectus. Duis condimentum tellus pellentesque turpis consequat ornare. Integer posuere dignissim quam, in vehicula orci maximus quis. Nunc sed arcu a lorem consequat feugiat in a sapien. ",
           url: "/",
           img: "https://picsum.photos/700/400",
+          startDate: "Junio 12, 2022",
+          tags: ["#trading", "#binance"],
         },
         {
           title: "Opciones binarias",
           description: "Duis condimentum tellus pellentesque turpis consequat ornare. Integer posuere dignissim quam, in vehicula orci maximus quis. Nunc sed arcu a lorem consequat feugiat in a sapien. ",
           url: "/goarbit",
           img: "https://www.urbeconomica.com.mx/images/2020/criptos.jpg",
+          startDate: "Agosto 3, 2022",
+          tags: ["#trading", "#bitcoin"],
         },
         {
           title: "¿Qué es GoArbit?",
           description: "Nunc malesuada euismod lectus. Integer posuere dignissim quam, in vehicula orci maximus quis. Nunc sed arcu a lorem consequat feugiat in a sapien. ",
           url: "/s",
           img: "https://picsum.photos/600/500",
+          startDate: "Junio 17, 2022",
+          tags: ["#trading", "#exchange"],
         },
         {
           title: "¿Qué es GoArbit? 22",
           description: "Nunc malesuada euismod lectus. Duis condimentum tellus pellentesque turpis consequat ornare. Integer posuere dignissim quam, in vehicula orci maximus quis. ",
           url: "/s",
           img: "https://picsum.photos/700/600",
+          startDate: "Septiembre 19, 2022",
+          tags: ["#trading", "#binance"],
         },
         {
           title: "¿Qué es GoArbit? 33",
           description: "Nunc malesuada euismod lectus. Duis condimentum tellus pellentesque turpis consequat ornare. Integer posuere dignissim quam, in vehicula orci maximus quis. Nunc sed arcu a lorem consequat feugiat in a sapien. ",
           url: "/s",
           img: "https://picsum.photos/800/900",
+          startDate: "Enero 24, 2023",
+          tags: ["#trading", "#binance"],
         },
       ]
     }
