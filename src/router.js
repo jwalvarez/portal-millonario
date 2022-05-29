8d8e696308fe195ddb44ef74de97beb9893332d9
Jhon Álvarez <jwalvarez.98@gmail.com>
2022-05-29 00:54:47 -0500

    added perfil to router config

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import Home from "./components/Home.vue";
 import Goarbit from "./components/Goarbit.vue";
 import Login from "./components/Login.vue";
 import Register from "./components/Register.vue";
+import Perfil from "./components/Perfil.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/goarbit", component: Goarbit },
   { path: "/register", component: Register },
   { path: "/login", component: Login },
+  { path: "/perfil", component: Perfil },
 ];
 
 const history = createWebHistory();
